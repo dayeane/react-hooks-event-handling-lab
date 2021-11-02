f882df99d80831bec4a80b814da880a4e530a2a1
Dayeane <dayeane85@gmail.com>
2021-11-01 18:39:36 -0700

    event handling in react lab

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,20 @@
 // Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe(props) {
+    function onFoc() {
+     console.log("Good!");   
+    }
+
+    function onBlu() {
+     console.log("Hey! Eyes on me!");   
+    }
+
+    return( 
+       <button onFocus={onFoc} onBlur={onBlu}>Eyes on me</button>
+    )
+}
+
+
+export default EyesOnMe; 
+
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,18 @@
 // Code Keypad Component Here
+ import React from "react";
+
+ function Keypad(props) {
+     function handleChange(e) {
+      console.log("Entering password...");   
+     }
+
+     return( 
+        <div>
+            <label for="password">password: </label>
+            <input id="password" type="password" onChange={handleChange} />
+        </div>
+     )
+ }
+
+
+ export default Keypad; 
